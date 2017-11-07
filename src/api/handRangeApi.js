7177a541da626ea264d1761ab4b4c39fa1e53317
Cameron Blazevich <cameron.blazevich@gmail.com>
2017-11-06 22:20:13 -0600

    point the api to the hosted web app

--- src/api/handRangeApi.js
+++ src/api/handRangeApi.js
@@ -2,28 +2,34 @@ import Auth from "../services/authService";
 
 class HandRangeApi {
   static getHandRanges(authBearer) {
-    var request = new Request("http://localhost:50338/api/handRanges/", {
-      headers: {
-        Authorization: authBearer
-      },
-      method: "GET"
-    });
+    var request = new Request(
+      "http://www.handrangememorizer.com/api/handRanges/",
+      {
+        headers: {
+          Authorization: authBearer
+        },
+        method: "GET"
+      }
+    );
     return fetch(request).then(response => response.json());
   }
 
   static updateHandRange(handRange, authBearer) {
-    var request = new Request("http://localhost:50338/api/handRanges/", {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: authBearer
-      },
-      method: "POST",
-      body: JSON.stringify({
-        position: handRange.position,
-        hands: handRange.hands
-      })
-    });
+    var request = new Request(
+      "http://www.handrangememorizer.com/api/handRanges/",
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: authBearer
+        },
+        method: "POST",
+        body: JSON.stringify({
+          position: handRange.position,
+          hands: handRange.hands
+        })
+      }
+    );
 
     return fetch(request).then(response => response.json());
   }
--- src/api/positionApi.js
+++ src/api/positionApi.js
@@ -1,11 +1,14 @@
 class PositionApi {
   static getAllPositions(id_token) {
-    var request = new Request("http://localhost:50338/api/positions/", {
-      headers: {
-        Authorization: "Bearer " + id_token
-      },
-      method: "GET"
-    });
+    var request = new Request(
+      "http://www.handrangememorizer.com/api/positions/",
+      {
+        headers: {
+          Authorization: "Bearer " + id_token
+        },
+        method: "GET"
+      }
+    );
     return fetch(request)
       .then(response => response.json())
       .then(data => data.positions);
